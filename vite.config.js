f121b341ead44675a86d02bdb20eaf4ff6c7c410
SquaredIT <info@squaredit.de>
2026-03-25 21:50:15 +0100

    fixes

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,6 @@ export default defineConfig({
         aussenfassaden: resolve(__dirname, 'pages/aussenfassaden.html'),
         bodenbelaege: resolve(__dirname, 'pages/bodenbelaege.html'),
         waermedaemmung: resolve(__dirname, 'pages/waermedaemmung.html'),
-        farbberatung: resolve(__dirname, 'pages/farbberatung.html'),
         impressum: resolve(__dirname, 'pages/impressum.html'),
         datenschutz: resolve(__dirname, 'pages/datenschutz.html'),
       },
